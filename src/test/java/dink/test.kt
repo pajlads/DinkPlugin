569c23e710604dc91c102ff8d8185e94b2ba7d1b
Sidd <iProdigy@users.noreply.github.com>
2022-10-13 12:51:32 -0700

    chore(test): ensure slayer task match is accurate

--- src/test/java/dink/test.kt
+++ src/test/java/dink/test.kt
@@ -1,24 +1,24 @@
 package dink
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
 import org.junit.jupiter.params.provider.ValueSource
 import universalDiscord.UniversalDiscordPlugin
+import java.util.stream.Stream
 
 class Matchers {
     @ParameterizedTest(name = "Slayer task completion message should trigger {0}")
-    @ValueSource(strings = [
-        "You have completed your task! You killed 125 Kalphite. You gained 11,150 xp.",
-        "You have completed your task! You killed 7 Ankous. You gained 75 xp.",
-        "You have completed your task! You killed 134 Abyssal demons. You gained 75 xp.",
-        "You have completed your task! You killed 134 Fossil Island Wyverns. You gained 75 xp.",
-        "You have completed your task! You killed 31 Kree'Arra. You gained 75 xp.",
-        "You have completed your task! You killed 31 TzKal-Zuk. You gained 75 xp.",
-    ])
-    fun `Slayer task completion regex finds match`(message: String) {
-        var matcher = UniversalDiscordPlugin.SLAYER_TASK_REGEX.matcher(message);
+    @ArgumentsSource(SlayerTaskProvider::class)
+    fun `Slayer task completion regex finds match`(message: String, task: String) {
+        val matcher = UniversalDiscordPlugin.SLAYER_TASK_REGEX.matcher(message)
         assertTrue(matcher.find())
+        assertEquals(task, matcher.group("task"))
     }
 
     @ParameterizedTest(name = "Slayer task completion message should trigger {0}")
@@ -28,7 +28,18 @@ class Matchers {
         "You've completed 234 tasks and received 15 points, giving you a total of 801; return to a Slayer master.",
     ])
     fun `Slayer task completion regex does not match`(message: String) {
-        var matcher = UniversalDiscordPlugin.SLAYER_TASK_REGEX.matcher(message);
+        val matcher = UniversalDiscordPlugin.SLAYER_TASK_REGEX.matcher(message)
         assertFalse(matcher.find())
+    }
+
+    private class SlayerTaskProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of("You have completed your task! You killed 125 Kalphite. You gained 11,150 xp.", "125 Kalphite"),
+            Arguments.of("You have completed your task! You killed 7 Ankous. You gained 75 xp.", "7 Ankous"),
+            Arguments.of("You have completed your task! You killed 134 Abyssal demons. You gained 75 xp.", "134 Abyssal demons"),
+            Arguments.of("You have completed your task! You killed 134 Fossil Island Wyverns. You gained 75 xp.", "134 Fossil Island Wyverns"),
+            Arguments.of("You have completed your task! You killed 31 Kree'Arra. You gained 75 xp.", "31 Kree'Arra"),
+            Arguments.of("You have completed your task! You killed 31 TzKal-Zuk. You gained 75 xp.", "31 TzKal-Zuk")
+        )
     }
 }
