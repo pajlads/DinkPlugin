7163d3471318993d9e66f9acdf16c1b56c96ddf6
Felanbird <41973452+Felanbird@users.noreply.github.com>
2022-10-14 15:45:29 -0400

    src/test/java/dinkplugin

--- src/test/java/dinkplugin/DinkTest.java
+++ src/test/java/dinkplugin/DinkTest.java
@@ -1,11 +1,10 @@
-package com.dink;
+package dinkplugin;
 
 import dinkplugin.DinkPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class DinkTest
-{
+public class DinkTest {
     public static void main(String[] args) throws Exception {
         ExternalPluginManager.loadBuiltin(DinkPlugin.class);
         RuneLite.main(args);
