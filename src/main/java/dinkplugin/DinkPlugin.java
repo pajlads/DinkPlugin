0bc895fa5da86a3d7a9297ad9b8cb40beea962bc
Felanbird <41973452+Felanbird@users.noreply.github.com>
2022-10-15 15:53:24 -0400

    remove duplicate import

--- src/main/java/dinkplugin/DinkPlugin.java
+++ src/main/java/dinkplugin/DinkPlugin.java
@@ -11,7 +11,6 @@ import net.runelite.api.events.*;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetID;
 import net.runelite.api.widgets.WidgetInfo;
-import net.runelite.client.events.NotificationFired;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.NotificationFired;
