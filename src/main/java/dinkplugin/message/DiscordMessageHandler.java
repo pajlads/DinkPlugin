c38d14c83f2c0e191236300d900ebc0486fce31f
Sidd <iProdigy@users.noreply.github.com>
2022-10-23 22:25:35 -0700

    refactor: simplify DiscordMessageHandler

--- src/main/java/dinkplugin/message/DiscordMessageHandler.java
+++ src/main/java/dinkplugin/message/DiscordMessageHandler.java
@@ -1,10 +1,10 @@
 package dinkplugin.message;
 
-import com.google.common.base.Strings;
 import dinkplugin.DinkPlugin;
 import dinkplugin.Utils;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.util.ImageUtil;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
@@ -13,13 +13,14 @@ import okhttp3.MultipartBody;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import static net.runelite.http.api.RuneLiteAPI.GSON;
 
@@ -33,21 +34,17 @@ public class DiscordMessageHandler {
     }
 
     public <T> void createMessage(boolean sendImage, @NonNull NotificationBody<T> mBody) {
+        String webhookUrl = plugin.getConfig().discordWebhook();
+        if (StringUtils.isBlank(webhookUrl)) return;
+
+        Collection<HttpUrl> urlList = Arrays.stream(StringUtils.split(webhookUrl, '\n'))
+            .filter(StringUtils::isNotBlank)
+            .map(HttpUrl::parse)
+            .collect(Collectors.toList());
+        if (urlList.isEmpty()) return;
+
         if (mBody.getPlayerName() == null)
             mBody.setPlayerName(Utils.getPlayerName(plugin.getClient()));
-
-        String webhookUrl = plugin.getConfig().discordWebhook();
-        if (Strings.isNullOrEmpty(webhookUrl)) {
-            return;
-        }
-        List<HttpUrl> urlList = new ArrayList<>();
-        String[] strList = webhookUrl.split("\n");
-        for (String urlString : strList) {
-            if (urlString.isEmpty()) {
-                continue;
-            }
-            urlList.add(HttpUrl.parse(urlString));
-        }
 
         MultipartBody.Builder reqBodyBuilder = new MultipartBody.Builder()
             .setType(MultipartBody.FORM)
@@ -55,45 +52,30 @@ public class DiscordMessageHandler {
 
         if (sendImage) {
             plugin.getDrawManager().requestNextFrameListener(image -> {
-                BufferedImage bufferedImage = (BufferedImage) image;
-                byte[] imageBytes;
                 try {
-                    imageBytes = Utils.convertImageToByteArray(bufferedImage);
+                    byte[] imageBytes = Utils.convertImageToByteArray(ImageUtil.bufferedImageFromImage(image));
+
+                    reqBodyBuilder.addFormDataPart(
+                        "file",
+                        "collectionImage.png",
+                        RequestBody.create(
+                            MediaType.parse("image/png"),
+                            imageBytes
+                        )
+                    );
                 } catch (IOException e) {
                     log.warn("There was an error creating bytes from captured image", e);
-                    // Still send the message even if the image cannot be created
-                    sendToMultiple(urlList, reqBodyBuilder);
-                    return;
                 }
-
-
-                reqBodyBuilder.addFormDataPart(
-                    "file",
-                    "collectionImage.png",
-                    RequestBody.create(
-                        MediaType.parse("image/png"),
-                        imageBytes
-                    )
-                );
-                sendToMultiple(urlList, reqBodyBuilder);
             });
-            return;
         }
 
-        sendToMultiple(urlList, reqBodyBuilder);
-    }
-
-    private void sendToMultiple(List<HttpUrl> urls, MultipartBody.Builder requestBody) {
-        for (HttpUrl url : urls) {
-            sendMessage(url, requestBody);
-        }
+        urlList.forEach(url -> sendMessage(url, reqBodyBuilder));
     }
 
     private void sendMessage(HttpUrl url, MultipartBody.Builder requestBody) {
-        RequestBody body = requestBody.build();
         Request request = new Request.Builder()
             .url(url)
-            .post(body)
+            .post(requestBody.build())
             .build();
         plugin.getHttpClient().newCall(request).enqueue(new Callback() {
             @Override
