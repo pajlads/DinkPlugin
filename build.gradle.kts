4298b12c93537c25bd3600bdf13f6a7bd44a86db
iProdigy <iProdigy@users.noreply.github.com>
2022-10-26 00:25:48 -0700

    chore: only apply kotlin plugin to test task types (#45)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     java
-    id("org.jetbrains.kotlin.jvm") version "1.7.20"
+    id("org.jetbrains.kotlin.jvm") version "1.7.20" apply false
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("io.freefair.lombok") version "6.5.1"
 }
@@ -23,6 +23,9 @@ dependencies {
     // runelite has outdated version with CVEs
     implementation(group = "com.google.guava", name = "guava", version = "31.1-jre")
 
+    // avoid excluding jetbrains annotations
+    compileOnly(group = "org.jetbrains", name = "annotations", version = "23.0.0")
+
     val runeLiteVersion = "1.9.1"
     compileOnly(group = "net.runelite", name = "client", version = runeLiteVersion)
     testImplementation(group = "net.runelite", name = "client", version = runeLiteVersion)
@@ -30,6 +33,8 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.9.0"))
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter")
+
+    testImplementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib")
 }
 
 group = "dinkplugin"
@@ -39,6 +44,10 @@ tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     sourceCompatibility = "1.8"
     targetCompatibility = "1.8"
+}
+
+tasks.withType<Test> {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
 }
 
 tasks.test {
