25e2999f3c3f12cf460c833238eb26d59d195a9f
Sidd <iProdigy@users.noreply.github.com>
2022-10-26 04:36:33 -0700

    chore: convert kotlin tests to java

---
+++ src/test/java/dinkplugin/IgnoredWorldTest.java
@@ -0,0 +1,55 @@
+package dinkplugin;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.EnumSet;
+
+import net.runelite.api.WorldType;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IgnoredWorldTest {
+    @ParameterizedTest(name = "World should not be ignored {0}")
+    @ArgumentsSource(NonIgnoredWorldTypeProvider.class)
+    void worldShouldNotBeIgnored(Set<WorldType> worldType) {
+        assertFalse(Utils.isIgnoredWorld(worldType));
+    }
+
+    private static class NonIgnoredWorldTypeProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                Arguments.of(EnumSet.of(WorldType.MEMBERS)),
+                Arguments.of(EnumSet.noneOf(WorldType.class)),
+                Arguments.of(EnumSet.of(WorldType.SKILL_TOTAL)),
+                Arguments.of(EnumSet.of(WorldType.FRESH_START_WORLD))
+            );
+        }
+    }
+
+    @ParameterizedTest(name = "World should be ignored {0}")
+    @ArgumentsSource(IgnoredWorldTypeProvider.class)
+    void worldShouldBeIgnored(Set<WorldType> worldType) {
+        assertTrue(Utils.isIgnoredWorld(worldType));
+    }
+
+    private static class IgnoredWorldTypeProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                Arguments.of(EnumSet.of(WorldType.PVP_ARENA)),
+                Arguments.of(EnumSet.of(WorldType.QUEST_SPEEDRUNNING)),
+                Arguments.of(EnumSet.of(WorldType.NOSAVE_MODE)),
+                Arguments.of(EnumSet.of(WorldType.TOURNAMENT_WORLD)),
+                Arguments.of(EnumSet.of(WorldType.FRESH_START_WORLD, WorldType.NOSAVE_MODE))
+            );
+        }
+    }
+}
---
+++ src/test/java/dinkplugin/notifiers/MatchersTest.java
@@ -0,0 +1,127 @@
+package dinkplugin.notifiers;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.regex.Matcher;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MatchersTest {
+
+    @ParameterizedTest(name = "Slayer task completion message should trigger {0}")
+    @ArgumentsSource(SlayerTaskProvider.class)
+    void SlayerTaskCompletionRegexFindsMatch(String message, String task) {
+        Matcher matcher = SlayerNotifier.SLAYER_TASK_REGEX.matcher(message);
+        assertTrue(matcher.find());
+        assertEquals(task, matcher.group("task"));
+    }
+
+    @ParameterizedTest(name = "Slayer task completion message should trigger {0}")
+    @ValueSource(
+        strings = {
+            "Forsen: forsen",
+            "You're assigned to kill kalphite; only 3 more to go.",
+            "You've completed 234 tasks and received 15 points, giving you a total of 801; return to a Slayer master."
+        }
+    )
+    void SlayerTaskCompletionRegexDoesNotMatch(String message) {
+        Matcher matcher = SlayerNotifier.SLAYER_TASK_REGEX.matcher(message);
+        assertFalse(matcher.find());
+    }
+
+    private static class SlayerTaskProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                Arguments.of(
+                    "You have completed your task! You killed 125 Kalphite. You gained 11,150 xp.",
+                    "125 Kalphite"
+                ),
+                Arguments.of("You have completed your task! You killed 7 Ankous. You gained 75 xp.", "7 Ankous"),
+                Arguments.of(
+                    "You have completed your task! You killed 134 Abyssal demons. You gained 75 xp.",
+                    "134 Abyssal demons"
+                ),
+                Arguments.of(
+                    "You have completed your task! You killed 134 Fossil Island Wyverns. You gained 75 xp.",
+                    "134 Fossil Island Wyverns"
+                ),
+                Arguments.of("You have completed your task! You killed 31 Kree'Arra. You gained 75 xp.", "31 Kree'Arra"),
+                Arguments.of("You have completed your task! You killed 31 TzKal-Zuk. You gained 75 xp.", "31 TzKal-Zuk")
+            );
+        }
+    }
+
+    @ParameterizedTest(name = "Collection log message should trigger {0}")
+    @ArgumentsSource(CollectionLogProvider.class)
+    void collectionLogRegexFindsMatch(String message, String item) {
+        Matcher matcher = CollectionNotifier.COLLECTION_LOG_REGEX.matcher(message);
+        assertTrue(matcher.find());
+        assertEquals(item, matcher.group("itemName"));
+    }
+
+    @ParameterizedTest(name = "Collection log message should not trigger {0}")
+    @ValueSource(
+        strings = {
+            "Forsen: forsen" // todo: add more bad examples
+        }
+    )
+    void CollectionLogRegexDoesNotMatch(String message) {
+        Matcher matcher = CollectionNotifier.COLLECTION_LOG_REGEX.matcher(message);
+        assertFalse(matcher.find());
+    }
+
+    private static class CollectionLogProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                Arguments.of("New item added to your collection log: Red d'hide body (t)",
+                    "Red d'hide body (t)"),
+                Arguments.of("New item added to your collection log: Rune full helm (g)",
+                    "Rune full helm (g)"),
+                Arguments.of("New item added to your collection log: Robin hood hat",
+                    "Robin hood hat"),
+                Arguments.of("New item added to your collection log: Amulet of glory (t4)",
+                    "Amulet of glory (t4)"),
+                Arguments.of("New item added to your collection log: Blue d'hide chaps (t)",
+                    "Blue d'hide chaps (t)"),
+                Arguments.of("New item added to your collection log: Lumberjack boots",
+                    "Lumberjack boots")
+            );
+        }
+    }
+
+    @ParameterizedTest(name = "Pet message should trigger {0}")
+    @ValueSource(
+        strings = {
+            "You have a funny feeling like you're being followed.",
+            "You have a funny feeling like you would have been followed...",
+            "You feel something weird sneaking into your backpack."
+        }
+    )
+    void PetRegexFindsMatch(String message) {
+        Matcher matcher = PetNotifier.PET_REGEX.matcher(message);
+        assertTrue(matcher.find());
+    }
+
+    @ParameterizedTest(name = "Pet message should not trigger {0}")
+    @ValueSource(
+        strings = {
+            "Forsen: forsen",
+            "You feel like you forgot to turn the stove off"
+        }
+    )
+    void PetRegexDoesNotMatch(String message) {
+        Matcher matcher = PetNotifier.PET_REGEX.matcher(message);
+        assertFalse(matcher.find());
+    }
+
+}
