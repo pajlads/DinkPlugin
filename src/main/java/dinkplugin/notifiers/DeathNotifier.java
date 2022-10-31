4c88b805f5f10a495a464768c237eb1aa317fc9e
Mm2PL <miau@mail.kotmisia.pl>
2022-10-31 15:13:26 +0100

    Clean up a little

--- src/main/java/dinkplugin/notifiers/DeathNotifier.java
+++ src/main/java/dinkplugin/notifiers/DeathNotifier.java
@@ -11,7 +11,6 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import javax.inject.Inject;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
