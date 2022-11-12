de33b43168e85132c0377722805fd9aca79651dc
Sidd <iProdigy@users.noreply.github.com>
2022-11-11 18:25:05 -0800

    feat: add diary listener

--- src/main/java/dinkplugin/DinkPlugin.java
+++ src/main/java/dinkplugin/DinkPlugin.java
@@ -6,6 +6,7 @@ import dinkplugin.notifiers.ClueNotifier;
 import dinkplugin.notifiers.CollectionNotifier;
 import dinkplugin.notifiers.CombatTaskNotifier;
 import dinkplugin.notifiers.DeathNotifier;
+import dinkplugin.notifiers.DiaryNotifier;
 import dinkplugin.notifiers.KillCountNotifier;
 import dinkplugin.notifiers.LevelNotifier;
 import dinkplugin.notifiers.LootNotifier;
@@ -23,6 +24,7 @@ import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.StatChanged;
 import net.runelite.api.events.UsernameChanged;
+import net.runelite.api.events.VarbitChanged;
 import net.runelite.api.events.WidgetLoaded;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -74,6 +76,7 @@ public class DinkPlugin extends Plugin {
     private final SpeedrunNotifier speedrunNotifier = new SpeedrunNotifier(this);
     private final KillCountNotifier killCountNotifier = new KillCountNotifier(this);
     private final CombatTaskNotifier combatTaskNotifier = new CombatTaskNotifier(this);
+    private final DiaryNotifier diaryNotifier = new DiaryNotifier(this);
 
     @Override
     protected void startUp() {
@@ -98,6 +101,7 @@ public class DinkPlugin extends Plugin {
     @Subscribe
     public void onGameStateChanged(GameStateChanged gameStateChanged) {
         levelNotifier.onGameStateChanged(gameStateChanged);
+        diaryNotifier.onGameState(gameStateChanged);
     }
 
     @Subscribe
@@ -108,6 +112,7 @@ public class DinkPlugin extends Plugin {
     @Subscribe
     public void onGameTick(GameTick event) {
         levelNotifier.onTick();
+        diaryNotifier.onTick();
     }
 
     @Subscribe
@@ -143,6 +148,11 @@ public class DinkPlugin extends Plugin {
     @Subscribe
     public void onLootReceived(LootReceived lootReceived) {
         lootNotifier.onLootReceived(lootReceived);
+    }
+
+    @Subscribe
+    public void onVarbitChanged(VarbitChanged event) {
+        diaryNotifier.onVarbitChanged(event);
     }
 
     @Subscribe
---
+++ src/main/java/dinkplugin/notifiers/DiaryNotifier.java
@@ -0,0 +1,62 @@
+package dinkplugin.notifiers;
+
+import dinkplugin.DinkPlugin;
+import dinkplugin.domain.AchievementDiaries;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.VarbitChanged;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class DiaryNotifier extends BaseNotifier {
+    private static final Map<Integer, AchievementDiaries.Diary> DIARIES = AchievementDiaries.INSTANCE.getDiaries();
+    private final Map<Integer, Integer> diaryCompletionById = new HashMap<>();
+
+    @Inject
+    public DiaryNotifier(DinkPlugin plugin) {
+        super(plugin);
+    }
+
+    public void reset() {
+        this.diaryCompletionById.clear();
+    }
+
+    public void onGameState(GameStateChanged event) {
+        if (event.getGameState() == GameState.LOGIN_SCREEN)
+            this.reset();
+    }
+
+    public void onTick() {
+        // init diary completions
+        if (diaryCompletionById.isEmpty()) {
+            Client client = plugin.getClient();
+            for (Integer id : DIARIES.keySet()) {
+                int value = client.getVarbitValue(id);
+                if (value >= 0) {
+                    diaryCompletionById.put(id, value);
+                }
+            }
+        }
+    }
+
+    public void onVarbitChanged(VarbitChanged event) {
+        int id = event.getVarbitId();
+        AchievementDiaries.Diary diary = DIARIES.get(id);
+        if (diary == null) return;
+
+        int value = event.getValue();
+        Integer previous = diaryCompletionById.put(id, value);
+        if (previous != null && value > previous) {
+            this.handle(diary);
+        }
+    }
+
+    private void handle(AchievementDiaries.Diary diary) {
+        log.debug("Diary completion detected: {}", diary);
+    }
+}
