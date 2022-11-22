8a8f1fbfa7bf52e7273a7c1a3ca6ce622d495b2a
Sidd <iProdigy@users.noreply.github.com>
2022-11-22 02:17:17 -0800

    feat: add mocked collection notifier test

---
+++ src/test/java/dinkplugin/notifiers/CollectionNotifierTest.java
@@ -0,0 +1,59 @@
+package dinkplugin.notifiers;
+
+import dinkplugin.message.NotificationBody;
+import dinkplugin.message.NotificationType;
+import dinkplugin.notifiers.data.CollectionNotificationData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class CollectionNotifierTest extends MockedNotifierTest {
+
+    @InjectMocks
+    CollectionNotifier notifier;
+
+    @Override
+    @BeforeEach
+    protected void setUp() {
+        super.setUp();
+
+        // init config mocks
+        when(config.notifyCollectionLog()).thenReturn(true);
+        when(config.collectionSendImage()).thenReturn(false);
+        when(config.collectionNotifyMessage()).thenReturn("%USERNAME% has added %ITEM% to their collection");
+    }
+
+    @Test
+    void testNotify() {
+        String item = "Seercull";
+
+        // send fake message
+        notifier.onChatMessage("New item added to your collection log: " + item);
+
+        // verify handled
+        verify(messageHandler).createMessage(
+            false,
+            NotificationBody.builder()
+                .content(String.format("%s has added %s to their collection", PLAYER_NAME, item))
+                .extra(new CollectionNotificationData(item))
+                .type(NotificationType.COLLECTION)
+                .build()
+        );
+    }
+
+    @Test
+    void testIgnore() {
+        // send unrelated message
+        notifier.onChatMessage("New item added to your backpack: weed");
+
+        // ensure no notification occurred
+        verify(messageHandler, never()).createMessage(anyBoolean(), any());
+    }
+
+}
