4d3c952a0a75e5c9a1789009d1a45d6207dfa473
Sidd <gitProdigy@proton.me>
2022-12-20 09:28:44 -0600

    chore: add more test cases

--- src/test/java/dinkplugin/notifiers/SlayerNotifierTest.java
+++ src/test/java/dinkplugin/notifiers/SlayerNotifierTest.java
@@ -49,7 +49,7 @@ class SlayerNotifierTest extends MockedNotifierTest {
     }
 
     @Test
-    void testNotifySpecial() {
+    void testNotifyBoss() {
         // fire chat messages
         notifier.onChatMessage("You are granted an extra reward of 5k Slayer XP for completing your boss task against the Cave Kraken boss.");
         notifier.onChatMessage("You have completed your task! You killed 50 Bosses. You gained 14,200 xp.");
@@ -62,6 +62,44 @@ class SlayerNotifierTest extends MockedNotifierTest {
             NotificationBody.builder()
                 .content(String.format("%s has completed: %s, getting %d points for a total %d tasks completed", PLAYER_NAME, "50 Cave Kraken", 10, 150))
                 .extra(new SlayerNotificationData("50 Cave Kraken", "150", "10"))
+                .type(NotificationType.SLAYER)
+                .build()
+        );
+    }
+
+    @Test
+    void testNotifyBarrows() {
+        // fire chat messages
+        notifier.onChatMessage("You are granted an extra reward of 5k Slayer XP for completing your boss task against Barrows brothers.");
+        notifier.onChatMessage("You have completed your task! You killed 36 Bosses. You gained 8,943 xp.");
+        notifier.onChatMessage("You've completed 881 tasks and received 20 points, giving you a total of 689; return to a Slayer master.");
+
+        // check notification message
+        verify(messageHandler).createMessage(
+            PRIMARY_WEBHOOK_URL,
+            false,
+            NotificationBody.builder()
+                .content(String.format("%s has completed: %s, getting %d points for a total %d tasks completed", PLAYER_NAME, "36 Barrows brothers", 20, 881))
+                .extra(new SlayerNotificationData("36 Barrows brothers", "881", "20"))
+                .type(NotificationType.SLAYER)
+                .build()
+        );
+    }
+
+    @Test
+    void testNotifyChaos() {
+        // fire chat messages
+        notifier.onChatMessage("You are granted an extra reward of 5k Slayer XP for completing your boss task against the Chaos Elemental.");
+        notifier.onChatMessage("You have completed your task! You killed 11 Bosses. You gained 7,954 xp.");
+        notifier.onChatMessage("You've completed 242 tasks and received 15 points, giving you a total of 3,254; return to a Slayer master.");
+
+        // check notification message
+        verify(messageHandler).createMessage(
+            PRIMARY_WEBHOOK_URL,
+            false,
+            NotificationBody.builder()
+                .content(String.format("%s has completed: %s, getting %d points for a total %d tasks completed", PLAYER_NAME, "11 Chaos Elemental", 15, 242))
+                .extra(new SlayerNotificationData("11 Chaos Elemental", "242", "15"))
                 .type(NotificationType.SLAYER)
                 .build()
         );
