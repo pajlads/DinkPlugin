a1b882b33e9a1c19f664e6a64b81daa7836519c1
Sidd <gitProdigy@proton.me>
2022-12-26 02:23:22 -0600

    docs: explain DurationAdapter

--- src/main/java/dinkplugin/util/DurationAdapter.java
+++ src/main/java/dinkplugin/util/DurationAdapter.java
@@ -8,6 +8,15 @@ import java.io.IOException;
 import java.time.Duration;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Serializes and deserializes {@link Duration} instances
+ * from their equivalent ISO-8601 string representation.
+ * <p>
+ * This adapter exists because GSON does not ship with
+ * a module for the Java 8 time API.
+ *
+ * @see <a href="https://github.com/google/gson/issues/1059">GSON Issue</>
+ */
 public class DurationAdapter extends TypeAdapter<Duration> {
     @Override
     public void write(JsonWriter out, Duration duration) throws IOException {
@@ -17,9 +26,8 @@ public class DurationAdapter extends TypeAdapter<Duration> {
     @Override
     public Duration read(JsonReader in) throws IOException {
         if (in.hasNext()) {
-            String str = in.nextString();
             try {
-                return Duration.parse(str);
+                return Duration.parse(in.nextString());
             } catch (DateTimeParseException ignored) {
             }
         }
