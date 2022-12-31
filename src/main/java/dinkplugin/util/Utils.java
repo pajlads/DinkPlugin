8462fbf8133db3d09cde042e6a2b297cca90b801
Sidd <gitProdigy@proton.me>
2022-12-30 23:47:00 -0600

    refactor: annotate Utils as UtilityClass

--- src/main/java/dinkplugin/util/Utils.java
+++ src/main/java/dinkplugin/util/Utils.java
@@ -1,5 +1,6 @@
 package dinkplugin.util;
 
+import lombok.experimental.UtilityClass;
 import net.runelite.api.Client;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
@@ -14,27 +15,28 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+@UtilityClass
 public class Utils {
 
-    public static final Color PINK = ColorUtil.fromHex("#f40098"); // analogous to RED in CIELCh_uv color space
-    public static final Color RED = ColorUtil.fromHex("#ca2a2d"); // red used in pajaW
+    public final Color PINK = ColorUtil.fromHex("#f40098"); // analogous to RED in CIELCh_uv color space
+    public final Color RED = ColorUtil.fromHex("#ca2a2d"); // red used in pajaW
 
-    public static boolean isSettingsOpen(@NotNull Client client) {
+    public boolean isSettingsOpen(@NotNull Client client) {
         Widget widget = client.getWidget(WidgetInfo.SETTINGS_INIT);
         return widget != null && !widget.isSelfHidden();
     }
 
-    public static String getPlayerName(Client client) {
+    public String getPlayerName(Client client) {
         return client.getLocalPlayer().getName();
     }
 
-    public static byte[] convertImageToByteArray(BufferedImage bufferedImage) throws IOException {
+    public byte[] convertImageToByteArray(BufferedImage bufferedImage) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
         return byteArrayOutputStream.toByteArray();
     }
 
-    public static boolean hasImage(@NotNull MultipartBody body) {
+    public boolean hasImage(@NotNull MultipartBody body) {
         return body.parts().stream().anyMatch(part -> {
             MediaType type = part.body().contentType();
             return type != null && "image".equals(type.type());
