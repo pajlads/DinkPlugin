8c4e7fd5f6f74ae928d90ab9462da7dc196ef1e5
Sidd <gitProdigy@proton.me>
2023-01-02 22:44:40 -0600

    chore: use smaller slayer thumbnail

--- src/main/java/dinkplugin/message/NotificationType.java
+++ src/main/java/dinkplugin/message/NotificationType.java
@@ -15,7 +15,7 @@ public enum NotificationType {
     LOOT("Loot Drop", "lootImage.png", WIKI_IMG_URL + "Rare_drop_table.png"),
     PET("Pet Obtained", "petImage.png", WIKI_IMG_URL + "Call_follower.png"),
     QUEST("Quest Completed", "questImage.png", WIKI_IMG_URL + "Quest_point_icon.png"),
-    SLAYER("Slayer Task", "slayerImage.png", WIKI_IMG_URL + "Slayer_icon_%28detail%29.png"),
+    SLAYER("Slayer Task", "slayerImage.png", WIKI_IMG_URL + "Slayer_icon.png"),
     SPEEDRUN("Quest Speedrunning", "speedrunImage.png", WIKI_IMG_URL + "Giant_stopwatch.png"),
     KILL_COUNT("Completion Count", "killCountImage.png", WIKI_IMG_URL + "Enchanted_gem.png"),
     COMBAT_ACHIEVEMENT("Combat Achievement", "combatTaskImage.png", WIKI_IMG_URL + "Combat_achievements.png"),
