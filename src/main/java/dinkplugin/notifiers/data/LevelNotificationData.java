be8eeca40cdce34af27c4acc38c5fd1f32bab318
Sidd <gitProdigy@proton.me>
2023-01-08 08:33:45 -0800

    chore: remove maxed skills field

--- src/main/java/dinkplugin/notifiers/data/LevelNotificationData.java
+++ src/main/java/dinkplugin/notifiers/data/LevelNotificationData.java
@@ -1,11 +1,8 @@
 package dinkplugin.notifiers.data;
 
-import dinkplugin.message.Field;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @Value
@@ -13,18 +10,4 @@ import java.util.Map;
 public class LevelNotificationData extends NotificationData {
     Map<String, Integer> levelledSkills;
     Map<String, Integer> allSkills;
-
-    @Override
-    public List<Field> getFields() {
-        if (levelledSkills.values().stream().anyMatch(lvl -> lvl == 99)) {
-            return Collections.singletonList(
-                new Field(
-                    "Total 99+ Skills",
-                    String.format("```\n%d\n```", allSkills.values().stream().filter(lvl -> lvl >= 99).count())
-                )
-            );
-        }
-
-        return super.getFields();
-    }
 }
