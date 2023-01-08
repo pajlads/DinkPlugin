4a14e7d1dd2004004f531d51f845e38bbc16a7f5
Sidd <gitProdigy@proton.me>
2023-01-08 15:16:21 -0800

    refactor: switch to okhttp execute in DiscordMessageHandler

--- src/main/java/dinkplugin/message/DiscordMessageHandler.java
+++ src/main/java/dinkplugin/message/DiscordMessageHandler.java
@@ -10,8 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.client.ui.DrawManager;
 import net.runelite.client.util.ImageUtil;
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.MediaType;
@@ -26,14 +24,12 @@ import org.jetbrains.annotations.VisibleForTesting;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -127,52 +123,40 @@ public class DiscordMessageHandler {
 
     private void sendMessage(HttpUrl url, MultipartBody.Builder requestBody, int attempt) {
         executor.execute(() -> {
-            CountDownLatch latch = new CountDownLatch(1);
             Request request = new Request.Builder()
                 .url(url)
                 .post(requestBody.build())
                 .build();
-            httpClient.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                    log.trace(String.format("Failed to send webhook message to %s on attempt %d", url, attempt), e);
 
-                    if (attempt == 0) {
-                        log.warn("There was an error sending the webhook message", e);
-                    }
-
-                    int maxRetries = config.maxRetries();
-                    if (attempt < maxRetries) {
-                        long baseDelay = config.baseRetryDelay();
-                        if (baseDelay > 0) {
-                            long delay = baseDelay * (1L << Math.min(attempt, 16)); // exponential backoff
-                            executor.schedule(() -> sendMessage(url, requestBody, attempt + 1), delay, TimeUnit.MILLISECONDS);
-                            log.debug("Scheduled webhook message for retry in {} milliseconds", delay);
-                        } else {
-                            log.debug("Skipping retry attempts for failed webhook since base delay is not positive");
-                        }
-                    } else if (maxRetries > 0) {
-                        log.warn("Exhausted retry attempts when sending the webhook message", e);
-                    } else {
-                        log.debug("Skipping retry attempts for failed webhook since max retries is not positive");
-                    }
-
-                    latch.countDown();
-                }
-
-                @Override
-                public void onResponse(@NotNull Call call, @NotNull Response response) {
+            try (Response response = httpClient.newCall(request).execute()) {
+                if (response.isSuccessful()) {
                     log.trace("Successfully sent webhook message to {} after {} attempts", url, attempt + 1);
-                    response.close();
-
-                    latch.countDown();
+                } else {
+                    String body = response.body() != null ? response.body().string() : null;
+                    throw new RuntimeException(String.format("Received unsuccessful http response: %d - %s - %s", response.code(), response.message(), body));
                 }
-            });
+            } catch (Exception e) {
+                log.trace(String.format("Failed to send webhook message to %s on attempt %d", url, attempt), e);
 
-            try {
-                latch.await();
-            } catch (InterruptedException e) {
-                log.warn("Failed to await webhook post completion", e);
+                if (attempt == 0) {
+                    log.warn("There was an error sending the webhook message", e);
+                }
+
+                int maxRetries = config.maxRetries();
+                if (attempt < maxRetries) {
+                    long baseDelay = config.baseRetryDelay();
+                    if (baseDelay > 0) {
+                        long delay = baseDelay * (1L << Math.min(attempt, 16)); // exponential backoff
+                        executor.schedule(() -> sendMessage(url, requestBody, attempt + 1), delay, TimeUnit.MILLISECONDS);
+                        log.debug("Scheduled webhook message for retry in {} milliseconds", delay);
+                    } else {
+                        log.debug("Skipping retry attempts for failed webhook since base delay is not positive");
+                    }
+                } else if (maxRetries > 0) {
+                    log.warn("Exhausted retry attempts when sending the webhook message", e);
+                } else {
+                    log.debug("Skipping retry attempts for failed webhook since max retries is not positive");
+                }
             }
         });
     }
