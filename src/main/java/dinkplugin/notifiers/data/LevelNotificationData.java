354cd4c078a1d6c6020a919819c58f463c731b59
Sidd <gitProdigy@proton.me>
2023-01-09 09:47:13 -0800

    feat: add maxed skills field on 99 level up

--- src/main/java/dinkplugin/notifiers/data/LevelNotificationData.java
+++ src/main/java/dinkplugin/notifiers/data/LevelNotificationData.java
@@ -1,8 +1,11 @@
 package dinkplugin.notifiers.data;
 
+import dinkplugin.message.Field;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @Value
@@ -10,4 +13,18 @@ import java.util.Map;
 public class LevelNotificationData extends NotificationData {
     Map<String, Integer> levelledSkills;
     Map<String, Integer> allSkills;
+
+    @Override
+    public List<Field> getFields() {
+        if (levelledSkills.containsValue(99)) {
+            return Collections.singletonList(
+                new Field(
+                    "Total 99+ Skills",
+                    String.format("```\n%d\n```", allSkills.values().stream().filter(lvl -> lvl >= 99).count())
+                )
+            );
+        }
+
+        return super.getFields();
+    }
 }
