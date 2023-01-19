a7268d30151620fee4722b3afdbfade0583fa970
Sidd <gitProdigy@proton.me>
2023-01-18 17:31:36 -0800

    feat: allow loot notifier to skip clue scrolls

--- src/test/java/dinkplugin/notifiers/LootNotifierTest.java
+++ src/test/java/dinkplugin/notifiers/LootNotifierTest.java
@@ -55,6 +55,7 @@ class LootNotifierTest extends MockedNotifierTest {
         when(config.lootIcons()).thenReturn(false);
         when(config.minLootValue()).thenReturn(500);
         when(config.includePlayerLoot()).thenReturn(true);
+        when(config.lootIncludeClueScrolls()).thenReturn(true);
         when(config.lootNotifyMessage()).thenReturn("%USERNAME% has looted: %LOOT% from %SOURCE% for %TOTAL_VALUE% gp");
 
         // init client mocks
