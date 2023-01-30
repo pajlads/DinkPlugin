ca8d4023225230191ed5501196df9222a0e7e300
iProdigy <gitProdigy@proton.me>
2023-01-30 09:39:46 -0800

    docs: explain empty string
    Co-authored-by: pajlada <rasmus.karlsson@pajlada.com>

--- src/main/java/dinkplugin/notifiers/PetNotifier.java
+++ src/main/java/dinkplugin/notifiers/PetNotifier.java
@@ -38,6 +38,7 @@ public class PetNotifier extends BaseNotifier {
 
     public void onChatMessage(String chatMessage) {
         if (petName == null && isEnabled() && PET_REGEX.matcher(chatMessage).matches()) {
+            // Prime the notifier to trigger next tick
             this.petName = "";
         }
     }
