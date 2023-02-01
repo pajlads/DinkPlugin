28670760a65d6f28a635f9ab02d199f2d84a3a34
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-02-01 21:48:21 +0100

    test: Multiple possible pet-name-sources
    this might be an overzealous test

--- src/test/java/dinkplugin/notifiers/PetNotifierTest.java
+++ src/test/java/dinkplugin/notifiers/PetNotifierTest.java
@@ -133,6 +133,39 @@ class PetNotifierTest extends MockedNotifierTest {
     }
 
     @Test
+    void testNotifyMultipleSameName() {
+        String petName = "TzRek-Jad";
+        int itemId = ItemID.TZREKJAD;
+
+        // prepare mocks
+        when(itemSearcher.findItemId("Tzrek-jad")).thenReturn(itemId);
+
+        // send fake message
+        notifier.onChatMessage("You have a funny feeling like you're being followed.");
+        notifier.onChatMessage("Untradeable drop: " + petName);
+        notifier.onClanNotification(
+            String.format(
+                "[ClanName] %s has a funny feeling like he would have been followed: %s at 50 killcount",
+                PLAYER_NAME,
+                petName
+            )
+        );
+        notifier.onTick();
+
+        // verify handled
+        verify(messageHandler).createMessage(
+            PRIMARY_WEBHOOK_URL,
+            false,
+            NotificationBody.builder()
+                .extra(new PetNotificationData(petName))
+                .text(PLAYER_NAME + " got a pet")
+                .thumbnailUrl(ItemUtils.getItemImageUrl(itemId))
+                .type(NotificationType.PET)
+                .build()
+        );
+    }
+
+    @Test
     void testNotifyClan() {
         String petName = "TzRek-Jad";
         int itemId = ItemID.TZREKJAD;
