b41a2ddaa34ab8cbe05aa739e4438f3d60125316
iProdigy <gitProdigy@proton.me>
2023-01-31 19:46:38 -0800

    feat: parse pet name from clan message (#149)
    Co-authored-by: Felanbird <41973452+Felanbird@users.noreply.github.com>
    Co-authored-by: pajlada <rasmus.karlsson@pajlada.com>

--- src/main/java/dinkplugin/notifiers/PetNotifier.java
+++ src/main/java/dinkplugin/notifiers/PetNotifier.java
@@ -2,14 +2,33 @@ package dinkplugin.notifiers;
 
 import dinkplugin.message.NotificationBody;
 import dinkplugin.message.NotificationType;
+import dinkplugin.notifiers.data.PetNotificationData;
+import dinkplugin.util.ItemSearcher;
+import dinkplugin.util.ItemUtils;
 import dinkplugin.util.Utils;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.VisibleForTesting;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Optional;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Singleton
 public class PetNotifier extends BaseNotifier {
     @VisibleForTesting
     static final Pattern PET_REGEX = Pattern.compile("You (?:have a funny feeling like you|feel something weird sneaking).*");
+
+    @VisibleForTesting
+    static final Pattern CLAN_REGEX = Pattern.compile("\\b(?<user>[\\w\\s]+) (?:has a funny feeling like .+ followed|feels something weird sneaking into .+ backpack): (?<pet>.+) at\\s");
+
+    private static final String PRIMED_NAME = "";
+
+    @Inject
+    private ItemSearcher itemSearcher;
+
+    private String petName = null;
 
     @Override
     public boolean isEnabled() {
@@ -23,17 +42,55 @@ public class PetNotifier extends BaseNotifier {
 
     public void onChatMessage(String chatMessage) {
         if (isEnabled() && PET_REGEX.matcher(chatMessage).matches()) {
-            this.handleNotify();
+            // Prime the notifier to trigger next tick
+            this.petName = PRIMED_NAME;
         }
+    }
+
+    public void onClanNotification(String message) {
+        if (!PRIMED_NAME.equals(petName)) {
+            // We have not received the normal message about a pet drop, so this clan message cannot be relevant to us
+            return;
+        }
+
+        Matcher matcher = CLAN_REGEX.matcher(message);
+        if (matcher.find()) {
+            String user = matcher.group("user").trim();
+            if (user.equals(Utils.getPlayerName(client))) {
+                this.petName = matcher.group("pet");
+            }
+        }
+    }
+
+    public void onTick() {
+        if (petName != null)
+            this.handleNotify();
+    }
+
+    public void reset() {
+        this.petName = null;
     }
 
     private void handleNotify() {
         String notifyMessage = config.petNotifyMessage()
             .replace("%USERNAME%", Utils.getPlayerName(client));
 
+        String thumbnail = Optional.ofNullable(petName)
+            .filter(s -> !s.isEmpty())
+            .map(Utils::ucFirst)
+            .map(itemSearcher::findItemId)
+            .map(ItemUtils::getItemImageUrl)
+            .orElse(null);
+
+        PetNotificationData extra = new PetNotificationData(StringUtils.defaultIfEmpty(petName, null));
+
         createMessage(config.petSendImage(), NotificationBody.builder()
+            .extra(extra)
             .text(notifyMessage)
+            .thumbnailUrl(thumbnail)
             .type(NotificationType.PET)
             .build());
+
+        reset();
     }
 }
