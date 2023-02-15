a6c207b9a10fa3913deaa5018b6a0e0fd340e3dc
Sidd <gitProdigy@proton.me>
2023-02-14 17:52:29 -0800

    feat: parse pet acquisition milestone

--- src/main/java/dinkplugin/notifiers/data/PetNotificationData.java
+++ src/main/java/dinkplugin/notifiers/data/PetNotificationData.java
@@ -4,6 +4,7 @@ import dinkplugin.message.Field;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,14 +12,16 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = false)
 public class PetNotificationData extends NotificationData {
     String petName;
+    String milestone;
 
     @Override
     public List<Field> getFields() {
         if (petName == null || petName.isEmpty())
             return super.getFields();
 
-        return Collections.singletonList(
-            new Field("Name", "```\n" + petName + "\n```")
-        );
+        List<Field> fields = new ArrayList<>();
+        fields.add(new Field("Name", "```\n" + petName + "\n```"));
+        if (milestone != null) fields.add(new Field("Milestone", "```\n" + milestone + "\n```"));
+        return fields;
     }
 }
