316cd3c9717e80f285bd911cb1a9daa6c8dac14f
Sidd <gitProdigy@proton.me>
2023-02-22 08:32:14 -0800

    refactor: simplify percentage formatting

--- src/main/java/dinkplugin/notifiers/data/CollectionNotificationData.java
+++ src/main/java/dinkplugin/notifiers/data/CollectionNotificationData.java
@@ -6,16 +6,12 @@ import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.List;
 
 @Value
 @EqualsAndHashCode(callSuper = false)
 public class CollectionNotificationData extends NotificationData {
-    // DecimalFormat is not thread-safe, so ThreadLocal is used.
-    // Currently, this is not necessary, but is simple future-proofing in case of a DiscordMessageHandler refactor.
-    private static final ThreadLocal<DecimalFormat> PERCENT_FORMAT = ThreadLocal.withInitial(() -> new DecimalFormat("#.#%"));
 
     @NotNull
     String itemName;
@@ -35,11 +31,11 @@ public class CollectionNotificationData extends NotificationData {
     @Override
     public List<Field> getFields() {
         if (completedEntries != null && totalEntries != null) {
-            String percent = PERCENT_FORMAT.get().format(1.0 * completedEntries / totalEntries);
+            double percent = 100.0 * completedEntries / totalEntries;
             return Collections.singletonList(
                 new Field(
                     "Completed Entries",
-                    Field.formatBlock("", String.format("%d/%d (%s)", completedEntries, totalEntries, percent)))
+                    Field.formatBlock("", String.format("%d/%d (%.1f%%)", completedEntries, totalEntries, percent)))
             );
         }
 
