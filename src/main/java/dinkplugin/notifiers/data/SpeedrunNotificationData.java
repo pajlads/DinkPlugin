436e27956a16c6ace130955c0326fc3aa01c15aa
pajlada <rasmus.karlsson@pajlada.com>
2023-10-09 07:21:12 +0200

    fix: stop treating Quest speedrun PB ties as personal bests (#329)
    Co-authored-by: iProdigy <8106344+iProdigy@users.noreply.github.com>

--- src/main/java/dinkplugin/notifiers/data/SpeedrunNotificationData.java
+++ src/main/java/dinkplugin/notifiers/data/SpeedrunNotificationData.java
@@ -2,11 +2,32 @@ package dinkplugin.notifiers.data;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.jetbrains.annotations.NotNull;
 
 @Value
 @EqualsAndHashCode(callSuper = false)
 public class SpeedrunNotificationData extends NotificationData {
+    /**
+     * The name of the quest (e.g. "Cook's Assistant")
+     */
+    @NotNull
     String questName;
+
+    /**
+     * The player's personal best of this quest
+     */
+    @NotNull
     String personalBest;
+
+    /**
+     * The time it took the player to finish the quest
+     */
+    @NotNull
     String currentTime;
+
+    /**
+     * Denotes whether this run was a new personal best or not.
+     * If the player ties with their previous personal best, this will be set to false.
+     */
+    boolean isPersonalBest;
 }
