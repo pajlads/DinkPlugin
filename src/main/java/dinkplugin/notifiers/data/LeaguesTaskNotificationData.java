404218eae14e314a64984b8adee91c27b048d39c
iProdigy <8106344+iProdigy@users.noreply.github.com>
2023-12-10 20:43:13 -0800

    chore: allow trophy info to supersede area info

--- src/main/java/dinkplugin/notifiers/data/LeaguesTaskNotificationData.java
+++ src/main/java/dinkplugin/notifiers/data/LeaguesTaskNotificationData.java
@@ -41,7 +41,7 @@ public class LeaguesTaskNotificationData extends NotificationData {
         List<Field> fields = new ArrayList<>(3);
         fields.add(new Field("Total Tasks", Field.formatBlock("", String.valueOf(tasksCompleted))));
         fields.add(new Field("Total Points", Field.formatBlock("", String.valueOf(totalPoints))));
-        if (tasksUntilNextArea != null) {
+        if (earnedTrophy == null && tasksUntilNextArea != null) {
             fields.add(new Field("Tasks until next Area", Field.formatBlock("", String.valueOf(tasksUntilNextArea))));
         } else if (earnedTrophy == null && pointsUntilNextRelic != null) {
             fields.add(new Field("Points until next Relic", Field.formatBlock("", String.valueOf(pointsUntilNextRelic))));
