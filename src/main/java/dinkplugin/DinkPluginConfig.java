d2b24703828255d56316964d27d69e10b5d490da
iProdigy <gitprodigy@proton.me>
2024-06-08 10:34:47 -0700

    chore: disable notificationfired by default

--- src/main/java/dinkplugin/DinkPluginConfig.java
+++ src/main/java/dinkplugin/DinkPluginConfig.java
@@ -1891,7 +1891,7 @@ public interface DinkPluginConfig extends Config {
         section = chatSection
     )
     default Set<ChatNotificationType> chatMessageTypes() {
-        return EnumSet.of(ChatNotificationType.GAME, ChatNotificationType.COMMAND, ChatNotificationType.RUNELITE);
+        return EnumSet.of(ChatNotificationType.GAME, ChatNotificationType.COMMAND);
     }
 
     @ConfigItem(
