f5db1a9346e1ed064c6ee2176ff6f33e591ee008
iProdigy <8106344+iProdigy@users.noreply.github.com>
2024-11-27 07:40:33 -0600

    chore: manually update relic tier points (#607)

--- src/main/java/dinkplugin/notifiers/LeaguesNotifier.java
+++ src/main/java/dinkplugin/notifiers/LeaguesNotifier.java
@@ -293,6 +293,7 @@ public class LeaguesNotifier extends BaseNotifier {
                 log.warn("Failed to initialize trophies", e);
             }
 
+            /*
             try {
                 initRelics(leaguesStruct);
                 log.debug("Relics: {}", TIER_BY_RELIC);
@@ -300,6 +301,7 @@ public class LeaguesNotifier extends BaseNotifier {
             } catch (Exception e) {
                 log.warn("Failed to initialize relics", e);
             }
+             */
         });
     }
 
@@ -331,13 +333,11 @@ public class LeaguesNotifier extends BaseNotifier {
             var tierStruct = client.getStructComposition(tierStructs[tierIndex - 1]);
             pointsMap.put(tierStruct.getIntValue(877), tier);
 
-            /*
             var relicStructs = client.getEnum(tierStruct.getIntValue(878)).getIntVals();
             for (int relicStruct : relicStructs) {
                 var name = client.getStructComposition(relicStruct).getStringValue(879);
                 TIER_BY_RELIC.put(name, tier);
             }
-            */
         }
         if (pointsMap.size() > 1) {
             TIER_BY_POINTS.clear();
