44ea5998d26f2cb113e7d74a1d8747e23cd539ec
iProdigy <gitprodigy@proton.me>
2024-12-22 21:11:18 -0600

    feat: add loot criteria to extra metadata

--- src/main/java/dinkplugin/notifiers/data/RareItemStack.java
+++ src/main/java/dinkplugin/notifiers/data/RareItemStack.java
@@ -1,21 +1,24 @@
 package dinkplugin.notifiers.data;
 
+import dinkplugin.domain.LootCriteria;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.Set;
+
 @Getter
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class RareItemStack extends SerializedItemStack {
+public class RareItemStack extends AnnotatedItemStack {
     private final double rarity;
 
-    public RareItemStack(int id, int quantity, int priceEach, String name, double rarity) {
-        super(id, quantity, priceEach, name);
+    public RareItemStack(int id, int quantity, int priceEach, String name, Set<LootCriteria> criteria, double rarity) {
+        super(id, quantity, priceEach, name, criteria);
         this.rarity = rarity;
     }
 
-    public static RareItemStack of(SerializedItemStack i, double rarity) {
-        return new RareItemStack(i.getId(), i.getQuantity(), i.getPriceEach(), i.getName(), rarity);
+    public static RareItemStack of(AnnotatedItemStack i, double rarity) {
+        return new RareItemStack(i.getId(), i.getQuantity(), i.getPriceEach(), i.getName(), i.getCriteria(), rarity);
     }
 }
