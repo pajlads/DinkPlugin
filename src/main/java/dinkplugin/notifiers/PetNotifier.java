b780fa0beeb55cef6ff14a626f8b6ef0ead2ce90
Swap <jd.anthdavis@gmail.com>
2025-05-12 21:57:58 -0400

    Add pet Huberte to PET_NAMES_TO_SOURCE
    Adds Huberte data to static Map PET_NAMES_TO_SOURCE in PetNotifier.

--- src/main/java/dinkplugin/notifiers/PetNotifier.java
+++ src/main/java/dinkplugin/notifiers/PetNotifier.java
@@ -380,6 +380,7 @@ public class PetNotifier extends BaseNotifier {
             entry("Baby mole", new KcSource("Giant Mole", 1.0 / 3_000)),
             entry("Baron", new KcSource("Duke Sucellus", 1.0 / 2_500)),
             entry("Bran", new MultiKcSource("Branda the Fire Queen", 1.0 / 3_000, "Eldric the Ice King", 1.0 / 3_000)), // assume loot not sacrificed
+            entry("Huberte", new KcSource("The Hueycoatl", 1.0 / 400)),
             entry("Butch", new KcSource("Vardorvis", 1.0 / 3_000)),
             entry("Beaver", new SkillSource(Skill.WOODCUTTING, 264_336, 85)), // teaks
             entry("Bloodhound", new KcSource("Clue Scroll (master)", 1.0 / 1_000)),
