9d2b6883ccd7aaa86ced22a485d5fdbb728c0fed
iProdigy <gitprodigy@proton.me>
2025-10-15 11:41:07 -0500

    fix: use seasonal policy for gridmaster worlds

--- src/main/java/dinkplugin/util/WorldTypeTracker.java
+++ src/main/java/dinkplugin/util/WorldTypeTracker.java
@@ -13,8 +13,17 @@ public class WorldTypeTracker extends BooleanStateTracker {
 
     @Override
     protected void populateState() {
+        int worldId = client.getWorld();
+        if (worldId < 0) {
+            return;
+        }
+
         Set<WorldType> world = client.getWorldType();
-        if (config.seasonalPolicy() == SeasonalPolicy.REJECT && world.contains(WorldType.SEASONAL)) {
+        String activity = client.getWorldList()[worldId].getActivity();
+        var seasonalPolicy = config.seasonalPolicy();
+        if (activity != null && activity.startsWith("Grid Master")) {
+            this.state = seasonalPolicy != SeasonalPolicy.REJECT;
+        } else if (seasonalPolicy == SeasonalPolicy.REJECT && world.contains(WorldType.SEASONAL)) {
             this.state = false;
         } else if (WorldUtils.isIgnoredWorld(world)) {
             this.state = false;
