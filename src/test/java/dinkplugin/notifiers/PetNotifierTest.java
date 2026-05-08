7f12c455f5ee0ee1569810517eeaa6e074f95883
iProdigy <gitprodigy@proton.me>
2026-05-07 21:00:28 -0500

    chore: add mock test

--- src/test/java/dinkplugin/notifiers/PetNotifierTest.java
+++ src/test/java/dinkplugin/notifiers/PetNotifierTest.java
@@ -464,6 +464,20 @@ class PetNotifierTest extends MockedNotifierTest {
     }
 
     @Test
+    void testIgnoreDuplicate() {
+        // update mocks
+        when(config.petIncludeDuplicates()).thenReturn(false);
+        when(config.petNotifyMessage()).thenReturn("%USERNAME% %GAME_MESSAGE%");
+
+        // send fake message
+        notifier.onChatMessage("You have a funny feeling like you would have been followed...");
+        IntStream.rangeClosed(0, MAX_TICKS_WAIT).forEach(i -> notifier.onTick());
+
+        // ensure no notification occurred
+        verify(messageHandler, never()).createMessage(any(), anyBoolean(), any());
+    }
+
+    @Test
     void testNotifyIrrelevantNameIgnore() {
         // ignore notifications for an irrelevant player
         when(config.filteredNames()).thenReturn("xqc");
